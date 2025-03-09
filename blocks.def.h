1228df6eb6553dc879c734f65e4b25c86284cecf
Mike Mehl <michaelalanmehl@gmail.com>
2025-03-09 11:17:24 -0400

    fix battery format

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-  {"🔋", "printf '%s%%' $(cat /sys/class/power_supply/BAT0/capacity)", 30, 0 },
+  {"Bat: ", "printf '%s%%' $(cat /sys/class/power_supply/BAT0/capacity)", 30, 0},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
